eb905b79712045ffd7a553060d8c9c298217553e
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-04-15 17:22:54 +0200

    add biobb_pdb_selchain test

---
+++ biobb_pdb_tools/test/unitests/test_pdb_tools/test_pdb_selchain.py
@@ -0,0 +1,18 @@
+# type: ignore
+from biobb_common.tools import test_fixtures as fx
+from biobb_pdb_tools.pdb_tools.biobb_pdb_chain import biobb_pdb_selchain
+
+
+class TestPdbChain():
+    def setup_class(self):
+        fx.test_setup(self, 'biobb_pdb_selchain')
+
+    def teardown_class(self):
+        fx.test_teardown(self)
+        pass
+
+    def test_pdbchain(self):
+        returncode = biobb_pdb_selchain(properties=self.properties, **self.paths)
+        assert fx.not_empty(self.paths['output_file_path'])
+        assert fx.equal(self.paths['output_file_path'], self.paths['ref_output_file_path'])
+        assert fx.exe_success(returncode)
