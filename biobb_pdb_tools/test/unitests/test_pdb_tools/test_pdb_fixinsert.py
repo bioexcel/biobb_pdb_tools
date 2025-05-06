e46b0133e3ebdb8deb187b7dc3c7c61ffaf96318
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-05-06 11:03:30 +0200

    add test_pdb_fixinsert

--- biobb_pdb_tools/test/unitests/test_pdb_tools/test_pdb_fixinsert.py
+++ biobb_pdb_tools/test/unitests/test_pdb_tools/test_pdb_fixinsert.py
@@ -1,18 +1,18 @@
 # type: ignore
 from biobb_common.tools import test_fixtures as fx
-from biobb_pdb_tools.pdb_tools.biobb_pdb_selchain import biobb_pdb_selchain
+from biobb_pdb_tools.pdb_tools.biobb_pdb_fixinsert import biobb_pdb_fixinsert
 
 
-class TestPdbSelChain():
+class TestFixInsert():
     def setup_class(self):
-        fx.test_setup(self, 'biobb_pdb_selchain')
+        fx.test_setup(self, 'biobb_pdb_fixinsert')
 
     def teardown_class(self):
         fx.test_teardown(self)
         pass
 
-    def test_pdb_selchain(self):
-        returncode = biobb_pdb_selchain(properties=self.properties, **self.paths)
+    def test_pdb_fixinsert(self):
+        returncode = biobb_pdb_fixinsert(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_file_path'])
         assert fx.equal(self.paths['output_file_path'], self.paths['ref_output_file_path'])
         assert fx.exe_success(returncode)
