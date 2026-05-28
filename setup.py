ee864f03c04ee05c01811527150ce1670cc5e521
gbayarri <genis.bayarri@irbbarcelona.org>
2026-05-28 16:05:06 +0200

    Pdb-tools only via pip

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     },
     packages=setuptools.find_packages(exclude=["docs", "test"]),
     package_data={"biobb_pdb_tools": ["py.typed"]},
-    install_requires=["biobb_common==5.2.2"],
+    install_requires=["biobb_common==5.2.2", "pdb-tools"],
     python_requires=">=3.10",
     entry_points={
         "console_scripts": [
